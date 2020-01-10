a6dc03c36ffb6a8a7a4d4f14ba8e980d5be1d760
asurygin <a.surigin@gmail.com>
2020-01-11 01:05:34 +0300

    update

--- monitor/monitor-setup.sh
+++ monitor/monitor-setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 cp /srv/monitor/watchlog /etc/sysconfig/watchlog
-ln -s /srv/monitor/watchlog.sh /usr/bin/watchlog.sh
-ln -s /srv/monitor/watchlog.service /etc/systemd/system/watchlog.service
-ln -s /srv/monitor/watchlog.timer /etc/systemd/system/watchlog.timer
+cp /srv/monitor/watchlog.sh /usr/bin/watchlog.sh
+cp /srv/monitor/watchlog.service /etc/systemd/system/watchlog.service
+cp /srv/monitor/watchlog.timer /etc/systemd/system/watchlog.timer
 systemctl daemon-reload
 
 cat /var/log/yum.log > /var/log/watchlog.log
 echo 'ALERT' >> /var/log/watchlog.log
 
 systemctl start watchlog.timer
+systemctl enable watchlog.timer
+systemctl enable watchlog.service
