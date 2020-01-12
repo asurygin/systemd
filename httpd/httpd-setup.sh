846bd4eefbcf38efb23b788ec71543fca7bce312
asurygin <a.surigin@gmail.com>
2020-01-12 23:29:31 +0300

    1

--- httpd/httpd-setup.sh
+++ httpd/httpd-setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 sed -i '/Listen 80/d' /etc/httpd/conf/httpd.conf
-cp /srv/httpd/httpd@.service /etc/systemd/system
-cp /srv/httpd/template.conf /etc/httpd/conf.d/template.conf
-cp /srv/httpd/httpd-first /etc/sysconfig
-cp /srv/httpd/httpd-second /etc/sysconfig
+cp /srv/httpd/httpd@.service /etc/systemd/system/httpd@.service
+cp /srv/httpd/httpd-first /etc/sysconfig/httpd-first
+cp /srv/httpd/httpd-second /etc/sysconfig/httpd-second
+cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/first.conf
+cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/second.conf
+sed -i 's/Listen 80/Listen 8080/' /etc/httpd/conf/second.conf
+sed -i '/ServerRoot "\/etc\/httpd"/a PidFile \/var\/run\/httpd-2.pid' /etc/httpd/conf/second.conf
 
+systemctl disable httpd
+systemctl daemon-reload
 systemctl enable httpd@service
 systemctl start httpd@first
- systemctl start httpd@second
+systemctl start httpd@second
